e2861a5727dd1ff3e4369a5a1d9bd8243470d65f
alistairosiris <16306760+alistairosiris@users.noreply.github.com>
2026-03-14 00:08:58 +0200

    added asteroid shooting

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from player import *
 from asteroid import *
 from asteroidfield import *
 import sys
+from shot import *
 
 
 def main():
@@ -18,10 +19,12 @@ def main():
     drawable = pygame.sprite.Group()
     updatable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Player.containers = (drawable, updatable)
     Asteroid.containers = (drawable, updatable, asteroids)
     AsteroidField.containers = (updatable)
+    Shot.containers = (drawable, updatable, shots)
 
     asteroid_field = AsteroidField()
 
@@ -37,13 +40,18 @@ def main():
 
         updatable.update(dt)
 
-        # check if any of the asteroids collide with the player
-
+        # check for asteroid collisions with the player or shots
         for a in asteroids:
             if a.collides_with(player1):
                 log_event("player_hit")
                 print("Game over!")
                 sys.exit()
+
+            for s in shots:
+                if a.collides_with(s):
+                    log_event("asteroid_shot")
+                    a.split()
+                    s.kill()
 
         for d in drawable:
             d.draw(screen)
